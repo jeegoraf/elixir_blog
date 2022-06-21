67b038619d0c25e76ce0e1fb99b5f61a2316a1a6
jeegoraf <ferenzlist90@gmail.com>
2022-06-21 23:27:45 +0300

    Added usable functions over Repo

--- lib/elixir_blog/repo.ex
+++ lib/elixir_blog/repo.ex
@@ -3,3 +3,38 @@ defmodule ElixirBlog.Repo do
     otp_app: :elixir_blog,
     adapter: Ecto.Adapters.Postgres
 end
+
+# функции с более удобным выводом и не возвращающие nil
+
+# возвращает единственный результат запроса
+def fetch_one(query) do
+  case __MODULE__.one(query) do
+    nil ->
+      {:error, :not_found}
+
+    value ->
+      {:ok, value}
+  end
+end
+
+# поиск по id
+def find(query, id) do
+  case __MODULE__.get(query, id) do
+    %_{} = schema ->
+      {:ok, schema}
+
+    nil ->
+      {:error, :not_found}
+  end
+end
+
+# поиск по условию
+def find_by(query, by) do
+  case __MODULE__.get_by(query, by) do
+    %_{} = schema ->
+      {:ok, schema}
+
+    nil ->
+      {:error, :not_found}
+  end
+end
