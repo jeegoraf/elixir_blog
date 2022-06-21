4e20143f2eec88ea17454642dbfef3e214c961c2
jeegoraf <ferenzlist90@gmail.com>
2022-06-21 23:56:42 +0300

    Fixed error in repo.ex

--- lib/elixir_blog/repo.ex
+++ lib/elixir_blog/repo.ex
@@ -2,39 +2,41 @@ defmodule ElixirBlog.Repo do
   use Ecto.Repo,
     otp_app: :elixir_blog,
     adapter: Ecto.Adapters.Postgres
+
+    # функции с более удобным выводом и не возвращающие nil
+
+    # возвращает единственный результат запроса
+    def fetch_one(query) do
+      case __MODULE__.one(query) do
+        nil ->
+          {:error, :not_found}
+
+        value ->
+          {:ok, value}
+      end
+    end
+
+    # поиск по id
+    def find(query, id) do
+      case __MODULE__.get(query, id) do
+        %_{} = schema ->
+          {:ok, schema}
+
+        nil ->
+          {:error, :not_found}
+      end
+    end
+
+    # поиск по условию
+    def find_by(query, by) do
+      case __MODULE__.get_by(query, by) do
+        %_{} = schema ->
+          {:ok, schema}
+
+        nil ->
+          {:error, :not_found}
+      end
+    end
+
 end
 
-# функции с более удобным выводом и не возвращающие nil
-
-# возвращает единственный результат запроса
-def fetch_one(query) do
-  case __MODULE__.one(query) do
-    nil ->
-      {:error, :not_found}
-
-    value ->
-      {:ok, value}
-  end
-end
-
-# поиск по id
-def find(query, id) do
-  case __MODULE__.get(query, id) do
-    %_{} = schema ->
-      {:ok, schema}
-
-    nil ->
-      {:error, :not_found}
-  end
-end
-
-# поиск по условию
-def find_by(query, by) do
-  case __MODULE__.get_by(query, by) do
-    %_{} = schema ->
-      {:ok, schema}
-
-    nil ->
-      {:error, :not_found}
-  end
-end
