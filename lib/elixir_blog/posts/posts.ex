bc470123ecb16abc5cfc2ec71ae0a9fdbc0cec19
jeegoraf <ferenzlist90@gmail.com>
2022-06-22 21:55:20 +0300

    Created posts context

---
+++ lib/elixir_blog/posts/posts.ex
@@ -0,0 +1,25 @@
+defmodule ElixirBlog.Posts do
+    @moduledoc """
+    Describes the context of the account
+    """
+  
+    alias ElixirBlog.Posts.Commands.{
+      CreatePost,
+      DeletePost,
+      UpdatePost
+    }
+  
+    alias ElixirBlog.Posts.Queries.{
+      GetPost,
+      ListPosts
+    }
+  
+    # Commands
+    defdelegate create_post(attrs), to: CreatePost, as: :process
+    defdelegate delete_post(post), to: DeletePost, as: :process
+    defdelegate update_post(post, attrs), to: UpdatePost, as: :process
+  
+    # Queries
+    defdelegate get_post(id), to: GetPost, as: :process
+    defdelegate list_posts(), to: ListUsers, as: :process
+  end
